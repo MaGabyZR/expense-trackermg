684f769087418e4c13b86f66cc1e80bbea1e350f
Gabriela Rubio <gabyrubioa@gmail.com>
2026-02-24 12:33:09 -0600

    Handling form submission.

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -21,14 +21,25 @@ const schema = z.object({
 //Create a TS type adn you are ready to plugin ReactHookForm
 type ExpenseFormData = z.infer<typeof schema>;
 
-const ExpenseForm = () => {
+//Handle Form submition.
+interface Props {
+  onSubmit: (data: ExpenseFormData) => void;
+}
+
+const ExpenseForm = ({ onSubmit }: Props) => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form
+      onSubmit={handleSubmit((data) => {
+        onSubmit(data);
+        reset();
+      })}
+    >
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
